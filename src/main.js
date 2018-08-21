d3860bca0433c067dd2745479d15720efbbebb08
Daniel <daniel@Daniels-MBP.fritz.box>
2018-08-21 18:39:28 +0200

    rounded application window

--- src/main.js
+++ src/main.js
@@ -6,12 +6,17 @@ app.on('ready', () => {
 
     mainWindow = new BrowserWindow({
         width: 500,
-        height: 500
+        height: 500,
+        resizable: false,
+        frame: false,
+        transparent: true
     });
 
     mainWindow.webContents.openDevTools();
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
 });
 
 app.on('quit', () => {
